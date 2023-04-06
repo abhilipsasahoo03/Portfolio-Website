52fa9d90304ed5ac31b3d244caf08cc78c43f5f5
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2023-04-06 22:55:04 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ export default function About(props) {
   return (
     <div class="top-0" id="aboutline">
       <section id="about">
-        <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center" id="aboutcont">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <TypeWriter />
             <br />
