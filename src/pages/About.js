7753a1a77d202e679a07ed314fe19eba4ef2ba73
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2023-04-06 22:16:13 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import TypeWriter from "../components/TypeWriter.jsx";
 
 export default function About(props) {
   return (
-    <div class="bg-white dark:bg-slate-800 top-0">
+    <div class="top-0" id="about">
       <section id="about">
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
