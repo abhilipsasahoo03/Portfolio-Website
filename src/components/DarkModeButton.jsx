2b767d17ccccdf9234303014970e0b2994cb1526
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2023-04-06 14:08:34 +0530

    Update DarkModeButton.jsx

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -2,49 +2,16 @@ import React from "react";
 
 export default function DarkModeToggleButton() {
   var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-  var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+  var tconst [darkToggle, setDarkToggle] = React.useState(false)
 
-  // Change the icons inside the button based on previous settings
-  if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    themeToggleLightIcon.classList.remove('hidden');
-  } else {
-    themeToggleDarkIcon.classList.remove('hidden');
-  }
-
-  var themeToggleBtn = document.getElementById('theme-toggle');
-
-  themeToggleBtn.addEventListener('click', function() {
-
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
-
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
-
-    // if NOT set via local storage previously
-    } else {
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        }
-    }
-    
-  });
   return (
-      <button id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-        <img alt="Sun Image" id="theme-toggle-light-icon" src="./sun.png" width="20" height="20" />
-        <img alt="Moon Image" id="theme-toggle-dark-icon" src="./moon.png" width="20" height="20" />
-      </button>
-  )
+    <div
+      class={`h-screen w-full flex items-center justify-center bg-gray-300 flex-col ${
+        darkToggle && 'dark'
+      }`}
+    >
+      <label class="toggleDarkBtn">
+        <input type="checkbox" onClick={() => setDarkToggle(!darkToggle)} />
+        <span class="slideBtnTg round"></span>
+      </label>
 }
