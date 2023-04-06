198689912131b14e5be56f0a121f6b7d8f64b6f1
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2023-04-06 11:41:36 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import TypeWriter from "./src/components/TypeWriter.jsx";
+import TypeWriter from "../components/TypeWriter.jsx";
 
 export default function About(props) {
   return (
