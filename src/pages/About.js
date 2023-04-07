9fdd4758a2b57cb4b2c7d18d9e8043e256edd63a
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2023-04-07 20:16:18 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,22 @@ import TypeWriter from "../components/TypeWriter.jsx";
 export default function About(props) {
   return (
     <div class="top-0" id="aboutline">
-      <img src="../assets/MyPic.jpg" className="justify-center" width="80" height="80" id="mypic" alt="My Pic" />
+     <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img
+              src="../assets/MyPic.jpg"
+              className="justify-center"
+              width="80"
+              height="80"
+              id="mypic"
+              alt="My Pic"
+            />
+            <br />
+            <img
+              className="object-cover object-center rounded"
+              alt="hero"
+              src="./MySignature.png"
+            />
+          </div>
       <section id="about">
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center" id="aboutcont">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
@@ -34,13 +49,7 @@ export default function About(props) {
               </a>
             </div>
           </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img
-              className="object-cover object-center rounded"
-              alt="hero"
-              src="./MySignature.png"
-            />
-          </div>
+          
         </div>
       </section>
     </div>
