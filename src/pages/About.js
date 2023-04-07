81d3a3241a71e756e3b86d95621ab972830b29e1
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2023-04-07 20:08:03 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ import TypeWriter from "../components/TypeWriter.jsx";
 export default function About(props) {
   return (
     <div class="top-0" id="aboutline">
-      <img src="./assets/MyPic.jpg" className="justify-center" width="80" height="80" id="mypic" alt="My Pic" />
+      <img src="../assets/MyPic.jpg" className="justify-center" width="80" height="80" id="mypic" alt="My Pic" />
       <section id="about">
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center" id="aboutcont">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
