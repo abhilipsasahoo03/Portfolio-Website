11ddca5c30909355346a036a982a054444aae0c9
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2023-04-12 13:00:22 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -29,8 +29,8 @@ export default function About(props) {
               Developer. Programmer. Open source enthusiast.
             </h2>
             <br />
-            <div className="descdiv flex justify-center rounded">
-              <p className="mb-8 leading-relaxed lg:text-4xl" id="desc">
+            <div className="descdiv flex items-center justify-center rounded">
+              <p className="mb-8 mt-8 leading-relaxed lg:text-4xl" id="desc">
                 Front-end Web Developer. Grace Hopper Celebration Scholar 2022.
                 The Shubhra Kar Linux Foundation Training Scholar 2022.
               </p>
