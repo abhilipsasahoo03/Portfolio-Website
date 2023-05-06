7fd8c769222e642fd3332cd8f92fd47fe5a21dd2
Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
2023-05-06 19:24:35 +0530

    Update BlogCard.js

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -7,8 +7,8 @@ const BlogCard = ({ post }) => {
             <img src={post.coverImage} alt={post.title} />
             <h2 className="font-extrabold">{post.title}</h2>
             <p>{post.brief}</p>
-            <br>
-            <br>
+            <br />
+            <br />
             <p className="font-extrabold">&#9829; {post.totalReactions} | &#128172; {post.responseCount}</p>
         </div>
     );
