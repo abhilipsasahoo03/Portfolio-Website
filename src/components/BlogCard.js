5b858d42a91689d9e72a735c13dfdbdd10acd2ab
Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
2023-05-07 13:38:51 +0530

    Update BlogCard.js

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -4,9 +4,10 @@ import '../App.css';
 const BlogCard = ({ post }) => {
     return (
         <div className="card">
-            <br />
-            <hr className="line"/>
-            <br />
+          <br />
+          <hr className="line"/>
+          <br />
+          <div className="cardcontent">
             <img src={post.coverImage} alt={post.title} />
             <br />
             <h2 className="font-extrabold">{post.title}</h2>
@@ -15,6 +16,7 @@ const BlogCard = ({ post }) => {
             <br />
             <br />
             <center><p className="font-extrabold">&#9829; {post.totalReactions} | &#128172; {post.responseCount}</p></center>
+         </div>
         </div>
     );
 }
