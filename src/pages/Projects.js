98d1092342b6762d4b2e9d6de177a10c31049f5c
Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
2023-05-17 20:33:25 +0530

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Projects() {
   return (
-  <div>
-  <h1>Projects</h1>
-  </div>
-  )
-};
+    <div>
+    <h1 className="title-font !bg-gradient-to-br !from-pink-300 !via-purple-300 !to-indigo-400 !text-4xl font-extrabold inline-block text-transparent bg-clip-text sm:text-5xl>Projects</h1>
+    </div>
+  );
+}
