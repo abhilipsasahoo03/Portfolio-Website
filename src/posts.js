a4a0c88b267e57cf2206a9e77d03c7a5969f2a0c
Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
2023-05-17 21:16:21 +0530

    Update posts.js

--- src/posts.js
+++ src/posts.js
@@ -1,15 +1,22 @@
-export const query = `
-    query GetUserArticles {
-        user(username: "abhilipsasahoo03") {
-            publication {
-                posts(page: ${page}) {
-                    coverImage
-                    title
-                    brief
-                    slug
-                    dateAdded
-                }
-            }
-        }
-    }
-`;
+const projectPosts = [
+  {
+    title: "FemmeCare",
+    link: "https://tech-monk-femmcare.vercel.app/",
+    coverImage: "https://user-images.githubusercontent.com/80174214/211193256-39bdaed6-b0c2-4efb-8fb0-e5745b15f11d.png",
+    description: "FemmeCare is a baby step towards prioritising and revolutionizing Women's health care. Resources page contains useful resources related to Women's reproductive and mental health, a real-time chat app using Socket.io facilitates community bonding and enabling women to share their stories with fellow women across the globe, and an integrated mental health tracker with interactive UI aids women to track their emotional and mental health in a fun way.
+
+The project is built using HTML, React.js, Tailwind CSS, Node.js, Express.js, Socket.io, Canva (for icons), Figma and PenPot (for creating wireframes).",
+  },
+  {
+    title: "Jordle",
+    link: "https://abhilipsasahoo03.github.io/Jordle/",
+    coverImage: "https://user-images.githubusercontent.com/80174214/239004435-4fd149b9-d332-407f-ae3b-7740f81fcf21.jpg",
+    description: "A Wordle Clone for you to play Wordle, anytime, anywhere. Now available with dark mode feature. Built using HTML, CSS and JavaScript along with animate.css and toastr for non-blocking notifications.",
+  },
+  {
+    title: "Personal Portfolio",
+    link: "https://csb-w3vic5.vercel.app/",
+    coverImage: "https://user-images.githubusercontent.com/80174214/239005513-e63df9ec-ffa6-4c72-b472-6ae37efc0eec.jpg",
+    description: "My personal portfolio project built using React, TailwindCSS and Hashnode's GraphQL API.",
+  }
+];
