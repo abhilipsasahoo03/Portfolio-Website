6ccab89cef92e4c672681bd400a82855a8ef56dd
Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
2023-05-17 20:42:58 +0530

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Projects() {
   return (
     <section id="projectsection">
-      <div>
+      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 container mx-auto flex flex-col items-center">
         <h1 id="projects" className="title-font !bg-gradient-to-br !from-pink-300 !via-purple-300 !to-indigo-400 !text-4xl font-extrabold inline-block text-transparent bg-clip-text sm:text-5xl">Projects</h1>
       </div>
     </section>
