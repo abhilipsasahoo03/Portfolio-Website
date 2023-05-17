b67d759cb94fe5806d53f6a6749fe8daafdd66f8
Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
2023-05-17 21:51:41 +0530

    Update projects.js

--- src/projects.js
+++ src/projects.js
@@ -3,13 +3,13 @@ export const project = [
     title: "FemmeCare",
     link: "https://tech-monk-femmcare.vercel.app/",
     coverImage: "https://user-images.githubusercontent.com/80174214/211193256-39bdaed6-b0c2-4efb-8fb0-e5745b15f11d.png",
-    description: "A baby step towards prioritising and revolutionizing Women's health care, containing resources page, realtime chat app and mental health tracker for women. Built using React.js, Tailwind CSS, Node.js, Express.js, Socket.io, Canva, PenPot.",
+    description: "Revolutionizing female healthcare. Contains resources, realtime chat app & mental health tracker. Built using React.js, TailwindCSS, Node.js, Express.js, Socket.io",
   },
   {
     title: "Jordle",
     link: "https://abhilipsasahoo03.github.io/Jordle/",
     coverImage: "https://user-images.githubusercontent.com/80174214/239004435-4fd149b9-d332-407f-ae3b-7740f81fcf21.jpg",
-    description: "A Wordle Clone for you to play Wordle, anytime, anywhere. Now available with dark mode feature. Built using HTML, CSS and JavaScript along with animate.css and toastr for non-blocking notifications.",
+    description: "A Wordle Clone with dark mode feature. Built using HTML, CSS and JavaScript with animate.css and toastr for non-blocking notifications.",
   },
   {
     title: "Personal Portfolio",
