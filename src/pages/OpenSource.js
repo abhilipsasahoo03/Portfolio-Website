6177993d1f66dc5f90fda0b4fc06f72a7e9e1395
Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
2023-05-23 22:10:27 +0530

    Update OpenSource.js

--- src/pages/OpenSource.js
+++ src/pages/OpenSource.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+export default function OpenSource() {
+  return (
+    <section id="opsrcsection">
+      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 container mx-auto flex flex-col items-center">
+        <h1 id="opsrc" className="title-font !bg-gradient-to-br !from-pink-300 !via-purple-300 !to-indigo-400 !text-4xl font-extrabold inline-block text-transparent bg-clip-text sm:text-5xl">Open Source</h1>
+      </div>
+      <br />
+    </section>
+  );
+}
