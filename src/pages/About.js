c23331ed0ac4a5e9bea22ff45f74c5584ecc5f23
Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
2023-05-24 23:14:07 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -31,7 +31,7 @@ export default function About(props) {
             </h2>
             <br />
             <div className="descdiv flex flex-wrap items-center justify-center rounded">
-              <div className="flex flex-row" id="icondiv">
+              <div className="flex flex-row gap-x-8 md:gap-x-16 lg:gap-x-32 text-3xl md:text-5xl lg:text-6xl" id="icondiv">
                 <a href="https://github.com/abhilipsasahoo03"><FontAwesomeIcon icon="fa-brands fa-github" id="icons" /></a>
                 <a href="https://linkedin.com/in/abhilipsasahoo"><FontAwesomeIcon icon="fa-brands fa-linkedin" id="icons" /></a>
                 <a href="https://www.twitter.com/_abelisaurus_"><FontAwesomeIcon icon="fa-brands fa-twitter" id="icons" /></a>
