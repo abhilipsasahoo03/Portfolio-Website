97529216325e2976456b3263ac863d870d445b35
Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
2023-05-27 18:31:22 +0530

    Update Navbar.js

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -14,7 +14,7 @@ export default function Navbar() {
               width="50%"
             />
           </a>
-        <nav className="md:mr-auto md:ml-3 md:py-1 md:border-l md:border-gray-700 flex flex-col gap-2 md:flex-row lg:flex-row text-base justify-center">
+        <nav className="md:mr-auto md:py-1 md:border-l flex flex-col gap-2 md:flex-row lg:flex-row text-base justify-center">
           <button className="mr-5 border border-black p-2 bg-white rounded-md navbutton hover:text-gray">
             <Link to="/">About</Link>
           </button>
