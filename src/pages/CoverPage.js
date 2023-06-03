6fea9f2c733ad447390e8b32c030e65fcfc70f6e
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2023-06-03 19:26:13 +0530

    Create CoverPage.js

---
+++ src/pages/CoverPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function CoverPage() {
+  return (
+    <div class="top-0">
+      <div className="marceline">
+        <p align="center">
+         <img
+            src="https://user-images.githubusercontent.com/80174214/147950792-fbd3df37-5ea9-454b-81c9-2cc82f7e00f1.gif"
+            width="75%"
+            alt="Floating Marceline is my persona"
+         />
+        </p>
+      </div>
+      <br />
+      <div class="welcome">
+        <p align="center" className="mb-8 ml-8 mr-8 w-full leading-relaxed lg:text-2xl" id="desc">
+          Hello, welcome to Abhilipsa's portfolio website. Click below to enter!
+        </p>
+      </div>
+    </div>
+  );
+}
