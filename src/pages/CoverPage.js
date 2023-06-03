8da73817586e546fec9888f11f4138264a8e8690
Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
2023-06-03 23:36:48 +0530

    Update CoverPage.js

--- src/pages/CoverPage.js
+++ src/pages/CoverPage.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 export default function CoverPage() {
   return (
    <>
-    <div className="content">  
-     <div class="top-0 coverimg">
+    <div className="top-0 content">  
+     <div class="coverimg">
       <div className="marceline">
         <p align="center">
          <img
