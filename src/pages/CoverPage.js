de95997616b1fa63a9434e9cb86baa1b1235bbca
Abhilipsa Sahoo <abhilipsasahoo03@outlook.com>
2023-06-03 22:56:59 +0530

    Update CoverPage.js

--- src/pages/CoverPage.js
+++ src/pages/CoverPage.js
@@ -17,7 +17,7 @@ export default function CoverPage() {
         </p>
       </div>
       <br />
-       <div className="descdiv flex flex-wrap items-center justify-center rounded">
+       <div className="introdiv flex flex-wrap items-center justify-center rounded">
         <p align="center" className="mb-8 ml-8 mr-8 w-full leading-relaxed lg:text-2xl" id="desc">
           Hello, welcome to Abhilipsa's portfolio website. Click <Link to="/Home/About">here</Link> to enter.
         </p>
