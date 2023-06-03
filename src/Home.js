dfc903e16335022fa13fbf146b51bb56e11bb8da
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2023-06-03 22:22:43 +0530

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import React from "react"; 
+ import About from "./pages/About.js"; 
+ import Navbar from "./pages/Navbar.js"; 
+ import Projects from "./pages/Projects.js"; 
+ import OpenSource from "./pages/OpenSource.js"; 
+ import Blogs from "./pages/Blogs.js"; 
+ import DarkModeToggle from "./components/DarkModeToggle"; 
+ import { Route, Routes } from "react-router-dom"; 
+  
+ export default function App() {  
+    return (  
+     <>  
+      <div className="main">  
+      </div>  
+      <main className="content">  
+        <Navbar />  
+        <center><DarkModeToggle /></center>  
+        <Routes>  
+          <Route path="/About" element={<About />}/>  
+          <Route path="/Blogs" element={<Blogs />}/>  
+          <Route path="/ 
+ Projects" element={<Projects />}/>  
+          <Route path="/OpenSource" element={<OpenSource />}/>  
+        </Routes>  
+      </main>  
+     </>  
+    );  
+  }
